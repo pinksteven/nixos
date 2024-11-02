835fe3fcc04784f99a54f777169218cd38d568bf
Steven <st.latuszek@gmail.com>
2024-11-02 14:38:49 +0100

    fix startup timing

--- common/home/system/hyprland/startup.nix
+++ common/home/system/hyprland/startup.nix
@@ -3,9 +3,9 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
         "steam -silent"
-        "vesktop --start-minimized"
+        "sleep 2; vesktop --start-minimized"
         "easyeffects --gapplication-service"
-        "spotify & sleep 1 && hyprctl dispatch focuswindow 'initialtitle:^(Spotify Premium)$' && hyprctl dispatch killactive"
+        "sleep 3; spotify & sleep 2 && hyprctl dispatch focuswindow 'initialtitle:^(Spotify Premium)$' && hyprctl dispatch killactive"
     ];
   };
 }
