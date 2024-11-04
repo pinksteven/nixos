45051e2ee8e2800830104fc8ac5fd993abf195e2
Steven <st.latuszek@gmail.com>
2024-11-04 09:54:28 +0100

    fix last commit

--- common/home/programs/spicetify/default.nix
+++ common/home/programs/spicetify/default.nix
@@ -31,7 +31,7 @@ in {
     enable = true;
     theme = spicePkgs.themes.dribbblish;
 
-    #colorScheme = "custom";
+    colorScheme = "custom";
 
     customColorScheme = {
       text               = base05;
--- common/home/system/hyprland/startup.nix
+++ common/home/system/hyprland/startup.nix
@@ -5,7 +5,6 @@
         "/etc/profiles/per-user/steven/bin/distrobox-enter  -n GameBox  --   steam -silent"
         "sleep 2; vesktop --start-minimized"
         "easyeffects --gapplication-service"
-        "sleep 4; spotify & sleep 2 && hyprctl dispatch focuswindow 'initialtitle:^(Spotify Premium)$' && hyprctl dispatch killactive"
         "syncthing --no-browser"
     ];
   };
