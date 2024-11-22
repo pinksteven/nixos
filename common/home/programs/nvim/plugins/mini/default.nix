23e230bdb012e431ce4458b1e24ad9277009d550
Steven <st.latuszek@gmail.com>
2024-11-22 10:41:15 +0100

    i haven't imported the file -_-

--- common/home/programs/nvim/plugins/mini/default.nix
+++ common/home/programs/nvim/plugins/mini/default.nix
@@ -1,29 +1,31 @@
 {
-    imports = [
-        ./surround.nix
-        ./bufremove.nix
-        ./comment.nix
-        ./diff.nix
-        ./indentscope.nix
-        ./fuzzy.nix
-        ./map.nix
-    ];
-    programs.nixvim.plugins = {
-        mini = {
-            enable = true;
-            mockDevIcons = true;
+  imports = [
+    ./surround.nix
+    ./bufremove.nix
+    ./comment.nix
+    ./diff.nix
+    ./indentscope.nix
+    ./fuzzy.nix
+    ./map.nix
+    ./starter.nix
+  ];
+  programs.nixvim.plugins = {
+    mini = {
+      enable = true;
+      mockDevIcons = true;
 
-            modules = {
-                icons = { };
-                ai = { };
-                cursorword = { };
-                operators = { };
-                pairs = { };
-                basics = { };
-                bracketed = { };
-                align = { };
-                git = { };
-            };
-        };
+      modules = {
+        icons = { };
+        ai = { };
+        cursorword = { };
+        operators = { };
+        pairs = { };
+        basics = { };
+        bracketed = { };
+        align = { };
+        git = { };
+      };
     };
+  };
 }
+
