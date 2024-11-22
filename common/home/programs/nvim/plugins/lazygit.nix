f45353fc146510a874ab3b008b8e6cfe3edc9ebb
Steven <st.latuszek@gmail.com>
2024-11-22 10:17:59 +0100

    lazygit outocmd change

--- common/home/programs/nvim/plugins/lazygit.nix
+++ common/home/programs/nvim/plugins/lazygit.nix
@@ -1,30 +1,31 @@
 {
-    programs.nixvim.plugins.lazygit = {
-        enable = true;
-    };
+  programs.nixvim.plugins.lazygit = {
+    enable = true;
+  };
 
-    programs.nixvim.keymaps = [
-        {
-            mode = "n";
-            key = "<leader>gl";
-            action = "<cmd>LazyGitCurrentFile<CR>";
-            options.desc = "Open lazygit";
-        }
-    ];
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gl";
+      action = "<cmd>LazyGitCurrentFile<CR>";
+      options.desc = "Open lazygit";
+    }
+  ];
 
-    programs.nixvim.autoCmd = [
-        {
-          event = "BufLeave";
-          pattern = "*lazygit*";
-          callback = { __raw = #Lua
+  programs.nixvim.autoCmd = [
+    {
+      event = "BufLeave";
+      pattern = "*lazygit*";
+      callback = {
+        __raw = # Lua
           ''
-            function()
-		      require("neo-tree.sources.filesystem.commands").refresh(
-			    require("neo-tree.sources.manager").get_state("filesystem")
-		      )
-		    end
+                      function()
+            		        require("neo-tree.sources.filesystem.commands").refresh(
+            			        require("neo-tree.sources.manager").get_state("filesystem")
+            		        )
+            		      end
           '';
-          };
-        }
-    ];
+      };
+    }
+  ];
 }
