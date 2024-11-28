72b344b290a8da5d1176feaffbadab921db83a06
Steven <st.latuszek@gmail.com>
2024-11-28 17:36:45 +0100

    remove custom rules till i have time to figure em out

--- common/nixos/sudo.nix
+++ common/nixos/sudo.nix
@@ -2,25 +2,5 @@
   security.sudo.enable = false;
   security.sudo-rs = {
     enable = true;
-    extraRules = [
-      {
-        groups = [ "ALL" ];
-        commands = [
-          {
-            command = "framework-tool --power";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "framework-tool --kblight";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "framework-tool --thermal";
-            options = [ "NOPASSWD" ];
-          }
-
-        ];
-      }
-    ];
   };
 }
