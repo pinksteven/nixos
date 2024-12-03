e33e0596e61ee4b2496b518857ca94a1f54002ef
Steven <st.latuszek@gmail.com>
2024-12-03 07:48:17 +0100

    fix steam startup

--- common/home/system/hyprland/startup.nix
+++ common/home/system/hyprland/startup.nix
@@ -1,11 +1,11 @@
-{ config, ... }:
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-        "/etc/profiles/per-user/steven/bin/distrobox-enter  -n GameBox  --   steam -silent"
-        "sleep 2; vesktop --start-minimized"
-        "easyeffects --gapplication-service"
-        "syncthing --no-browser"
+      "steam -silent"
+      "sleep 2; vesktop --start-minimized"
+      "easyeffects --gapplication-service"
+      "syncthing --no-browser"
     ];
   };
 }
+
