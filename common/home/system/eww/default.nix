1aa1fdc375105423fbcd37e54b4877acae541458
Steven <st.latuszek@gmail.com>
2025-01-13 19:06:28 +0100

    eww workspaces widget

--- common/home/system/eww/default.nix
+++ common/home/system/eww/default.nix
@@ -1,4 +1,15 @@
+{ pkgs, ... }:
 {
+  # Packages needed as dependencies mosetly for scripts
+  home.packages = with pkgs; [
+    gawk
+    coreutils
+    gnugrep
+    socat
+    jq
+    python3
+  ];
+
   programs.eww = {
     enable = true;
     enableZshIntegration = true;
