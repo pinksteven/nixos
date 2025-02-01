d07621cbf26f7e3af56cae10e576705714ca8154
Steven <st.latuszek@gmail.com>
2025-02-01 12:24:50 +0100

    back to tuigreet

--- common/nixos/greetd.nix
+++ common/nixos/greetd.nix
@@ -3,11 +3,32 @@ let
   theme = ''border=blue;text=yellow;prompt=green;action=cyan;button=magenta;container=black;input=white'';
 in
 {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --asterisks --asterisks-char • --window-padding 1 -t --time-format '%H:%M | %a  %d %b %Y' -g 'Welcome back!' -c 'Hyprland > /dev/null 2>&1' --theme '${theme}'";
+        user = "greeter";
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ greetd.tuigreet ];
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
   # To prevent getting stuck at shutdown
   systemd.extraConfig = "DefaultTimeoutStopSec=10s";
-
-  stylix.targets.regreet.enable = true;
-  programs.regreet = {
-    enable = true;
-  };
 }
