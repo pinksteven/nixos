db33c275ce48586fa484b61d30fe6ebb5bb2391c
Steven <st.latuszek@gmail.com>
2025-02-06 15:32:19 +0100

    not use hashed passwd (it breaks)

--- nixos/users.nix
+++ nixos/users.nix
@@ -8,9 +8,11 @@ in
   };
   users = {
     defaultUserShell = pkgs.zsh;
+    # mutableUsers = false;
     users.${user} = {
       isNormalUser = true;
       description = "${user} account";
+      # hashedPasswordFile = config.sops.secrets."passwords/steven".path;
       extraGroups = [
         "networkmanager"
         "wheel"
