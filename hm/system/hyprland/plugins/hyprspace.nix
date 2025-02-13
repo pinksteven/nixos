55e57300c85b2dc7ff6f6a9424d6e9bc616603f2
Steven <st.latuszek@gmail.com>
2025-02-13 14:03:54 +0100

    hyprspace is back on + flake.lock

--- hm/system/hyprland/plugins/hyprspace.nix
+++ hm/system/hyprland/plugins/hyprspace.nix
@@ -1,10 +1,46 @@
-{ inputs, pkgs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  rgb = color: "rgb(${color})";
+  rgba = color: alpha: "rgba(${color}${lib.toHexString (builtins.ceil (alpha * 255))})";
+  inherit (config.lib.stylix) colors;
+in
 {
   wayland.windowManager.hyprland = {
     plugins = [ inputs.hyprspace.packages.${pkgs.system}.Hyprspace ];
     settings = {
       plugin.overview = {
+        # BEHAVIOR
         autoDrag = false;
+        autoScroll = true;
+        switchOnDrop = true;
+        exitOnSwitch = false;
+        showEmptyWorkspace = true;
+
+        # LAYOUT
+        panelHeight = 250;
+        panelBorderWidth = 0;
+        workspaceMargin = 10;
+        workspaceBorderSize = 0;
+        centerAligned = true;
+        hideBackgroundLayers = false;
+        hideTopLayers = false;
+        hideOverlayLayers = true;
+        hideRealLayers = true;
+        drawActiveWorkspace = true;
+
+        # STYLING
+        panelColor = rgba colors.base00 0.1;
+        panelBorderColor = rgb colors.base0D;
+        workspaceActiveBorder = rgb colors.base05;
+        workspaceInactiveBorder = rgb colors.base01;
+        dragAlpha = 0.7;
+        disableBlur = false;
       };
       bind = [
         "$mod, TAB, overview:toggle" # Overview
