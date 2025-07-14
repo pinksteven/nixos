2b55b5de7f59212c300f1e10c0316bdcf727ef38
Steven <st.latuszek@gmail.com>
2025-07-14 14:40:51 +0200

    start hyprpolikitagent via systemd (from wiki)

--- hm/system/hyprland/polkitagent.nix
+++ hm/system/hyprland/polkitagent.nix
@@ -2,5 +2,5 @@
 {
   home.packages = [ pkgs.hyprpolkitagent ];
 
-  wayland.windowManager.hyprland.settings.exec-once = [ "systemctl --user start hyprpolkitagent" ];
+  # wayland.windowManager.hyprland.settings.exec-once = [ "systemctl --user start hyprpolkitagent" ];
 }
