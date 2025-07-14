f6e670a63d515af2487da2a307a4e11a483f578f
Steven <st.latuszek@gmail.com>
2025-07-14 14:42:05 +0200

    actualy enable hyprpolkitagent via hm module

--- hm/system/hyprland/polkitagent.nix
+++ hm/system/hyprland/polkitagent.nix
@@ -1,6 +1,5 @@
-{ pkgs, ... }:
 {
-  home.packages = [ pkgs.hyprpolkitagent ];
-
-  # wayland.windowManager.hyprland.settings.exec-once = [ "systemctl --user start hyprpolkitagent" ];
+  services.hyprpolkitagent = {
+    enable = true;
+  };
 }
