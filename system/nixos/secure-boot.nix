c1a78d2a19e894b2f30687c48b3daf3c85e1c3ce
Steven <st.latuszek@gmail.com>
2025-07-21 15:58:09 +0200

    fix misspelled systemd-boot module

--- system/nixos/secure-boot.nix
+++ system/nixos/secure-boot.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = [ pkgs.sbctl ];
 
-  boot.loader.systemdBoot.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
 
   boot.lanzaboote = {
     enable = true;
